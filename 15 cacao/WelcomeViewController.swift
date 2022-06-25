518a25bed3cd539099dabff68a778748fe29de79
Guizeppe2905 <mariagilina@yandex.ru>
2022-06-25 19:05:33 +0300

    navigationUpdated

--- 15 cacao/WelcomeViewController.swift
+++ 15 cacao/WelcomeViewController.swift
@@ -210,13 +210,24 @@ class WelcomeViewController: UIViewController {
         navigationController?.pushViewController(signInVC, animated: false)
     }
     @objc private func didTapUserButton() {
-        let swiftUIUserMode = UserMainContentView()
-        let userMode = UIHostingController(rootView: swiftUIUserMode)
-        navigationController?.pushViewController(userMode, animated: true)
+            let keyWindow = UIApplication.shared.connectedScenes
+               .filter({$0.activationState == .foregroundActive})
+               .map({$0 as? UIWindowScene})
+               .compactMap({$0})
+               .first?.windows
+               .filter({$0.isKeyWindow}).first
+           keyWindow!.rootViewController = UIHostingController(rootView: UserMainContentView())
+           keyWindow!.makeKeyAndVisible()
     }
     @objc func didTapBusinessButton() {
-   let containerVC = ContainerViewController()
-  navigationController?.pushViewController(containerVC, animated: true)
+        let keyWindow = UIApplication.shared.connectedScenes
+           .filter({$0.activationState == .foregroundActive})
+           .map({$0 as? UIWindowScene})
+           .compactMap({$0})
+           .first?.windows
+           .filter({$0.isKeyWindow}).first
+        keyWindow!.rootViewController = UINavigationController(rootViewController: ContainerViewController())
+       keyWindow!.makeKeyAndVisible()
     }
    private func setupConstraints () {
       imageView.snp.makeConstraints{ make in
